9dfc77ae27474b29af030f0c1e831a3fa8e4098a
Lzzya <lzzy201311@163.com>
2016-03-30 20:13:13 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-mydata <- read.table("d:/r/household_power_consumption.txt",header = TRUE,sep = ";",stringsAsFactors = FALSE)
+mydata <- read.table("household_power_consumption.txt",header = TRUE,sep = ";",stringsAsFactors = FALSE)
 mydata$Date <- as.Date(mydata$Date,"%d/%m/%Y")
 mydata1 <- subset(mydata,Date >="2007-02-01"&Date <="2007-02-02")
 mydata1$Time <- strptime(paste(mydata1$Date, mydata1$Time), "%Y-%m-%d %H:%M:%S")
