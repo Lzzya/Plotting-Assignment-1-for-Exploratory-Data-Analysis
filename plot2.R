6c09f10113b013832b9b3af8ed04c09b7b72d859
Lzzya <lzzy201311@163.com>
2016-03-30 17:08:32 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,8 +4,10 @@ mydata1 <- subset(mydata,Date >="2007-02-01"&Date <="2007-02-02")
 mydata1$Time <- strptime(paste(mydata1$Date, mydata1$Time), "%Y-%m-%d %H:%M:%S")
 mydata1$Global_active_power <- as.numeric(mydata1$Global_active_power)
 
+png("plot2.png")
 plot(mydata1$Time, mydata1$Global_active_power, 
      type="l",
      xlab = "",
      ylab = "Global Active Power(kilowatts)"
      )
+dev.off()
