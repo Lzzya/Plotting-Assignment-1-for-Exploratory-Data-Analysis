5ee21efe02f3c918d59b9d81d61a94f4992ea76c
Lzzya <lzzy201311@163.com>
2016-03-30 20:03:19 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -8,6 +8,16 @@ mydata1$Sub_metering_2 <- as.numeric(mydata1$Sub_metering_2)
 mydata1$Voltage <- as.numeric(mydata1$Voltage)
 mydata1$Global_reactive_power <- as.numeric(mydata1$Global_reactive_power)
 
+mydata <- read.table("d:/r/household_power_consumption.txt",header = TRUE,sep = ";",stringsAsFactors = FALSE)
+mydata$Date <- as.Date(mydata$Date,"%d/%m/%Y")
+mydata1 <- subset(mydata,Date >="2007-02-01"&Date <="2007-02-02")
+mydata1$Time <- strptime(paste(mydata1$Date, mydata1$Time), "%Y-%m-%d %H:%M:%S")
+mydata1$Global_active_power <- as.numeric(mydata1$Global_active_power)
+mydata1$Sub_metering_1 <- as.numeric(mydata1$Sub_metering_1)
+mydata1$Sub_metering_2 <- as.numeric(mydata1$Sub_metering_2)
+mydata1$Voltage <- as.numeric(mydata1$Voltage)
+mydata1$Global_reactive_power <- as.numeric(mydata1$Global_reactive_power)
+
 png("plot4.png")
 par(mfrow=c(2,2))
 plot(mydata1$Time, mydata1$Global_active_power, 
@@ -44,4 +54,5 @@ plot(mydata1$Time, mydata1$Global_reactive_power,
      xlab = "datetime",
      ylab = "Global_reactive_power"
 )
+
 dev.off()
