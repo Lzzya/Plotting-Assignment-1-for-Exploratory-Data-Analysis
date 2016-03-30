0f9bfae0ed8fbf6dbd60d4a0ecf5794ff4eab10a
Lzzya <lzzy201311@163.com>
2016-03-30 20:25:19 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 mydata <- read.table("household_power_consumption.txt",header = TRUE,sep = ";",stringsAsFactors = FALSE)
 mydata$Date <- as.Date(mydata$Date,"%d/%m/%Y")
 mydata1 <- subset(mydata,Date >="2007-02-01"&Date <="2007-02-02")
-
+mydata1$Global_active_power <- as.numeric(mydata1$Global_active_power)
 png("plot1.png")
 hist(mydata1$Global_active_power,
      col="red",
